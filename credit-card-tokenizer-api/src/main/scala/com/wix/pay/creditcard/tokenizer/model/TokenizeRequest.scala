34582211a2b03c3407f40ff29c1965974ff7722b
Raz, Ohad <ohadr@wix.com>
2015-08-03 16:38:55 +0300

    reviving... got lost in merge...

---
+++ credit-card-tokenizer-api/src/main/scala/com/wix/pay/creditcard/tokenizer/model/TokenizeRequest.scala
@@ -0,0 +1,17 @@
+/*      __ __ _____  __                                              *\
+**     / // // /_/ |/ /          Wix                                 **
+**    / // // / /|   /           (c) 2006-2015, Wix LTD.             **
+**   / // // / //   |            http://www.wix.com/                 **
+**   \__/|__/_//_/| |                                                **
+\*                |/                                                 */
+package com.wix.pay.creditcard.tokenizer.model
+
+
+import com.wix.pay.creditcard.CreditCard
+
+
+/** Encapsulates the data for a Tokenize request.
+  *
+  * @author <a href="mailto:ohadr@wix.com">Raz, Ohad</a>
+  */
+case class TokenizeRequest(creditCard: CreditCard)
